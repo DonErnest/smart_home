5017ecf15d2bd811ebb7a07b424f059c6064dbe2
Ernest Aitiev <valeetmihifaveas@gmail.com>
2025-05-02 22:48:57 +0600

    Added models for smart home widgets

---
+++ lib/models/gadget.dart
@@ -0,0 +1,75 @@
+import 'package:uuid/uuid.dart';
+
+const uuid = Uuid();
+
+enum GadgetStatus {
+  disconnected("Disconnected"),
+  active("Active"),
+  processing("Processing"),
+  alert("Alert");
+
+  final String displayStatus;
+
+  const GadgetStatus(this.displayStatus);
+}
+
+class Gadget {
+  late final String id;
+  final String name;
+  final GadgetStatus status;
+
+  Gadget({String? id, required this.name, required this.status})
+    : id = id ?? uuid.v4();
+}
+
+enum LightState {
+  off("Off"),
+  on("On"),
+  onMovement("Turned on movement");
+
+  final String displayStatus;
+
+  const LightState(this.displayStatus);
+}
+
+class Light extends Gadget {
+  final LightState state;
+
+  Light({required super.name, required super.status, required this.state});
+}
+
+enum ConditioningMode {
+  heating("Heating"),
+  cooling("Cooling"),
+  aeration("Aeration");
+
+  final String displayStatus;
+
+  const ConditioningMode(this.displayStatus);
+}
+
+class Conditioning extends Gadget {
+  final double temperature;
+  final ConditioningMode mode;
+
+  Conditioning({
+    required super.name,
+    required super.status,
+    required this.temperature,
+    required this.mode,
+  });
+}
+
+class Teapot extends Gadget {
+  final double waterLevelMl;
+  final double temperature;
+  final bool sendingDataToCIA;
+
+  Teapot({
+    required super.name,
+    required super.status,
+    required this.waterLevelMl,
+    required this.temperature,
+    required this.sendingDataToCIA,
+  });
+}
